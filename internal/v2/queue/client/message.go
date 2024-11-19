4cff4cfc3391e5715f581b10bb414e0b0cb26117
jeremy-babylonchain <jeremy.tsang@babylonchain.io>
2024-11-19 15:55:31 +0800

    resolve comments

--- internal/v2/queue/client/message.go
+++ internal/v2/queue/client/message.go
@@ -5,14 +5,8 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// commented the already initialized queue clients in v1
 func (q *V2QueueClient) StartReceivingMessages() {
 	log.Printf("Starting to receive messages from v2 queues")
-	// queueclient.StartQueueMessageProcessing(
-	// 	q.ActiveStakingEventQueueClient,
-	// 	q.Handler.ActiveStakingHandler, q.Handler.HandleUnprocessedMessage,
-	// 	q.MaxRetryAttempts, q.ProcessingTimeout,
-	// )
 
 	log.Printf("Starting to receive messages from verified staking queue")
 	queueclient.StartQueueMessageProcessing(
@@ -28,33 +22,11 @@ func (q *V2QueueClient) StartReceivingMessages() {
 		q.MaxRetryAttempts, q.ProcessingTimeout,
 	)
 
-	// log.Printf("Starting to receive messages from unbonding staking queue")
-	// queueclient.StartQueueMessageProcessing(
-	// 	q.UnbondingEventQueueClient,
-	// 	q.Handler.UnbondingStakingHandler, q.Handler.HandleUnprocessedMessage,
-	// 	q.MaxRetryAttempts, q.ProcessingTimeout,
-	// )
-
-	// log.Printf("Starting to receive messages from expired staking queue")
-	// queueclient.StartQueueMessageProcessing(
-	// 	q.StakingExpiredEventQueueClient,
-	// 	q.Handler.ExpiredStakingHandler, q.Handler.HandleUnprocessedMessage,
-	// 	q.MaxRetryAttempts, q.ProcessingTimeout,
-	// )
-
 }
 
 // Turn off all message processing
 func (q *V2QueueClient) StopReceivingMessages() {
 	log.Printf("Stopping to receive messages from v2 queues")
-
-	// log.Printf("Stopping to receive messages from active staking queue")
-	// activeQueueErr := q.ActiveStakingEventQueueClient.Stop()
-	// if activeQueueErr != nil {
-	// 	log.Error().Err(activeQueueErr).
-	// 		Str("queueName", q.ActiveStakingEventQueueClient.GetQueueName()).
-	// 		Msg("error while stopping queue")
-	// }
 
 	log.Printf("Stopping to receive messages from verified staking queue")
 	verifiedQueueErr := q.VerifiedStakingEventQueueClient.Stop()
@@ -71,20 +43,4 @@ func (q *V2QueueClient) StopReceivingMessages() {
 			Str("queueName", q.PendingStakingEventQueueClient.GetQueueName()).
 			Msg("error while stopping queue")
 	}
-
-	// log.Printf("Stopping to receive messages from unbonding staking queue")
-	// unbondingQueueErr := q.UnbondingEventQueueClient.Stop()
-	// if unbondingQueueErr != nil {
-	// 	log.Error().Err(unbondingQueueErr).
-	// 		Str("queueName", q.UnbondingEventQueueClient.GetQueueName()).
-	// 		Msg("error while stopping queue")
-	// }
-
-	// log.Printf("Stopping to receive messages from expired staking queue")
-	// expiredQueueErr := q.StakingExpiredEventQueueClient.Stop()
-	// if expiredQueueErr != nil {
-	// 	log.Error().Err(expiredQueueErr).
-	// 		Str("queueName", q.StakingExpiredEventQueueClient.GetQueueName()).
-	// 		Msg("error while stopping queue")
-	// }
 }
