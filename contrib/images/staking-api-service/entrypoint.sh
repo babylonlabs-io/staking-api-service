6d478d3a14f2af31ac989abab9dbf223f41816bb
Gurjot Singh <111540954+gusin13@users.noreply.github.com>
2024-11-19 17:35:42 +0530

    Revert "delegation allowlist (#121)" (#129)
    This reverts commit 1d22f9ad4d5214ce36cbc7229708f36c3a77b6c1.

--- contrib/images/staking-api-service/entrypoint.sh
+++ contrib/images/staking-api-service/entrypoint.sh
@@ -6,7 +6,6 @@ BINARY=${BINARY:-/bin/staking-api-service}
 CONFIG=${CONFIG:-/home/staking-api-service/config.yml}
 PARAMS=${PARAMS:-/home/staking-api-service/global-params.json}
 FINALITY_PROVIDERS=${FINALITY_PROVIDERS:-/home/staking-api-service/finality-providers.json}
-DELEGATION_ALLOWLIST=${DELEGATION_ALLOWLIST:-/home/staking-api-service/delegation-allowlist.json}
 
 if ! [ -f "${BINARY}" ]; then
 	echo "The binary $(basename "${BINARY}") cannot be found."
@@ -28,9 +27,4 @@ if ! [ -f "${FINALITY_PROVIDERS}" ]; then
 	exit 1
 fi
 
-if ! [ -f "${DELEGATION_ALLOWLIST}" ]; then
-	echo "The delegation allowlist file $(basename "${DELEGATION_ALLOWLIST}") cannot be found. Please add the delegation allowlist file to the shared folder. Use the DELEGATION_ALLOWLIST environment variable if the name of the delegation allowlist file is not 'delegation-allowlist.json'"
-	exit 1
-fi
-
-$BINARY --config "$CONFIG" --params "$PARAMS" --finality-providers "$FINALITY_PROVIDERS" --delegation-allowlist "$DELEGATION_ALLOWLIST" 2>&1
+$BINARY --config "$CONFIG" --params "$PARAMS" --finality-providers "$FINALITY_PROVIDERS" 2>&1
