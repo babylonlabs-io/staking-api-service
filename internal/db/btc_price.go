bfc61e2a13c48564287da0fdcbda2abd3c947c44
Gurjot <gurjotsyd@gmail.com>
2024-12-04 11:37:13 +0530

    pr comment: change get/set fns

--- internal/db/btc_price.go
+++ internal/db/btc_price.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/babylonlabs-io/staking-api-service/internal/db/model"
 )
@@ -13,7 +14,7 @@ func (db *Database) GetLatestBtcPrice(ctx context.Context) (*model.BtcPrice, err
 	client := db.Client.Database(db.DbName).Collection(model.BtcPriceCollection)
 
 	var btcPrice model.BtcPrice
-	err := client.FindOne(ctx, bson.M{}).Decode(&btcPrice)
+	err := client.FindOne(ctx, bson.M{"_id": model.BtcPriceDocID}).Decode(&btcPrice)
 	if err != nil {
 		return nil, err
 	}
@@ -24,17 +25,16 @@ func (db *Database) GetLatestBtcPrice(ctx context.Context) (*model.BtcPrice, err
 func (db *Database) SetBtcPrice(ctx context.Context, price float64) error {
 	client := db.Client.Database(db.DbName).Collection(model.BtcPriceCollection)
 
-	// Always store as a single document
-	_, err := client.DeleteMany(ctx, bson.M{})
-	if err != nil {
-		return err
-	}
-
 	btcPrice := model.BtcPrice{
+		ID:        model.BtcPriceDocID, // Fixed ID for single document
 		Price:     price,
-		CreatedAt: time.Now(),
+		CreatedAt: time.Now(), // For TTL index
 	}
 
-	_, err = client.InsertOne(ctx, btcPrice)
+	opts := options.Update().SetUpsert(true)
+	filter := bson.M{"_id": model.BtcPriceDocID}
+	update := bson.M{"$set": btcPrice}
+
+	_, err := client.UpdateOne(ctx, filter, update, opts)
 	return err
 }
