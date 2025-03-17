c990bcc9c608fc2bc997d26f3e5d372c9eaec228
Kirill <paltsev.kir@gmail.com>
2025-03-18 02:18:15 +0400

    Remove unused singleflight

--- internal/v2/service/service.go
+++ internal/v2/service/service.go
@@ -5,23 +5,20 @@ import (
 	dbclients "github.com/babylonlabs-io/staking-api-service/internal/shared/db/clients"
 	"github.com/babylonlabs-io/staking-api-service/internal/shared/http/clients"
 	"github.com/babylonlabs-io/staking-api-service/internal/shared/services/service"
-	"golang.org/x/sync/singleflight"
 )
 
 type V2Service struct {
-	dbClients         *dbclients.DbClients
-	clients           *clients.Clients
-	cfg               *config.Config
-	sharedService     *service.Service
-	singleFlightGroup *singleflight.Group
+	dbClients     *dbclients.DbClients
+	clients       *clients.Clients
+	cfg           *config.Config
+	sharedService *service.Service
 }
 
 func New(sharedService *service.Service) (*V2Service, error) {
 	return &V2Service{
-		dbClients:         sharedService.DbClients,
-		clients:           sharedService.Clients,
-		cfg:               sharedService.Cfg,
-		sharedService:     sharedService,
-		singleFlightGroup: &singleflight.Group{},
+		dbClients:     sharedService.DbClients,
+		clients:       sharedService.Clients,
+		cfg:           sharedService.Cfg,
+		sharedService: sharedService,
 	}, nil
 }
