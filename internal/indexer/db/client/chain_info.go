1512747ae46bd97fb34896ae97a3c2e613ba7f8a
Kirill <paltsev.kir@gmail.com>
2025-07-24 00:06:09 +0400

    Add network info logic

---
+++ internal/indexer/db/client/chain_info.go
@@ -0,0 +1,35 @@
+package indexerdbclient
+
+import (
+	"context"
+	"errors"
+	model "github.com/babylonlabs-io/staking-api-service/internal/indexer/db/model"
+	"github.com/babylonlabs-io/staking-api-service/internal/shared/db"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const networkInfoID = "singleton"
+
+type networkInfoDoc struct {
+	ID string `bson:"_id"`
+	*model.NetworkInfo
+}
+
+func (idb *IndexerDatabase) GetNetworkInfo(ctx context.Context) (*model.NetworkInfo, error) {
+	filter := map[string]any{"_id": networkInfoID}
+	res := idb.collection(model.NetworkInfoCollection).FindOne(ctx, filter)
+
+	var doc networkInfoDoc
+	err := res.Decode(&doc)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, &db.NotFoundError{
+				Key:     networkInfoID,
+				Message: "network info not found",
+			}
+		}
+		return nil, err
+	}
+
+	return doc.NetworkInfo, nil
+}
--- internal/indexer/db/model/bsn.go
+++ internal/indexer/db/model/bsn.go
@@ -1,12 +1,11 @@
 package indexerdbmodel
 
 type BSN struct {
-	ID                string         `bson:"_id"`
-	Name              string         `bson:"name"`
-	Description       string         `bson:"description"`
-	MaxMultiStakedFPS uint32         `bson:"max_multi_staked_fps"` // max number of finality providers from consumer
-	Type              string         `bson:"type"`
-	RollupMetadata    *ETHL2Metadata `bson:"rollup_metadata"`
+	ID             string         `bson:"_id"`
+	Name           string         `bson:"name"`
+	Description    string         `bson:"description"`
+	Type           string         `bson:"type"`
+	RollupMetadata *ETHL2Metadata `bson:"rollup_metadata"`
 }
 
 const (
