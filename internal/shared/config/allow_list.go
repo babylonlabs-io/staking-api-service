cd19409f371a38531de667c4df40c3bfe54c3587
Kirill <paltsev.kir@gmail.com>
2025-08-01 17:53:40 +0400

    Fix config validation

--- internal/shared/config/allow_list.go
+++ internal/shared/config/allow_list.go
@@ -7,7 +7,7 @@ type AllowList struct {
 }
 
 func (cfg *AllowList) Validate() error {
-	if cfg.ExpirationBlock > 0 {
+	if cfg.ExpirationBlock <= 0 {
 		return fmt.Errorf("allow-list: expiration block cannot be set to 0 or negative")
 	}
 
