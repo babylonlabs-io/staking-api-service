1a952917b1f037b566566d9543a96c530b839fbf
Kirill <paltsev.kir@gmail.com>
2025-08-01 17:55:21 +0400

    Rewrote impossible if condition in AllowList.Validate()

--- internal/shared/config/allow_list.go
+++ internal/shared/config/allow_list.go
@@ -7,8 +7,8 @@ type AllowList struct {
 }
 
 func (cfg *AllowList) Validate() error {
-	if cfg.ExpirationBlock <= 0 {
-		return fmt.Errorf("allow-list: expiration block cannot be set to 0 or negative")
+	if cfg.ExpirationBlock == 0 {
+		return fmt.Errorf("allow-list: expiration block cannot be zero")
 	}
 
 	return nil
