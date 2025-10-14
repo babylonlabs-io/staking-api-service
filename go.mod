5131f4e2b6ff007acdb0d9f0a2746ab28a847cb5
Kirill <paltsev.kir@gmail.com>
2025-10-15 03:26:53 +0400

    Add caching in /v2/apr (#426)

--- go.mod
+++ go.mod
@@ -139,6 +139,7 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/opencontainers/runc v1.2.5 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/petermattis/goid v0.0.0-20240813172612-4fcff4a6cae7 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
--- internal/v2/service/service.go
+++ internal/v2/service/service.go
@@ -1,13 +1,18 @@
 package v2service
 
 import (
+	"time"
+
 	"github.com/babylonlabs-io/staking-api-service/internal/shared/bbnclient"
 	"github.com/babylonlabs-io/staking-api-service/internal/shared/config"
 	dbclients "github.com/babylonlabs-io/staking-api-service/internal/shared/db/clients"
 	"github.com/babylonlabs-io/staking-api-service/internal/shared/http/clients"
 	"github.com/babylonlabs-io/staking-api-service/internal/shared/integrations/keybase"
 	"github.com/babylonlabs-io/staking-api-service/internal/shared/services/service"
+	"github.com/patrickmn/go-cache"
 )
+
+const aprCacheTTL = time.Minute
 
 type V2Service struct {
 	dbClients     *dbclients.DbClients
@@ -16,9 +21,13 @@ type V2Service struct {
 	sharedService *service.Service
 	keybaseClient *keybase.Client
 	bbnClient     *bbnclient.BBNClient
+	aprCache      *cache.Cache
 }
 
 func New(sharedService *service.Service, keybaseClient *keybase.Client, bbnClient *bbnclient.BBNClient) (*V2Service, error) {
+	// because there are no dynamic keys cleanupInterval=-1 which means there is no goroutine that removes expired items
+	aprCache := cache.New(aprCacheTTL, -1)
+
 	return &V2Service{
 		dbClients:     sharedService.DbClients,
 		clients:       sharedService.Clients,
@@ -26,5 +35,6 @@ func New(sharedService *service.Service, keybaseClient *keybase.Client, bbnClien
 		sharedService: sharedService,
 		keybaseClient: keybaseClient,
 		bbnClient:     bbnClient,
+		aprCache:      aprCache,
 	}, nil
 }
