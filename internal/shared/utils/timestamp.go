1c2746c51104a90f0ef0a32e6c623f58e8ae2764
necipsagiro <sagiroglun22@itu.edu.tr>
2025-12-23 23:54:25 +0300

    test: update timestamp parsing to UTC

--- internal/shared/utils/timestamp.go
+++ internal/shared/utils/timestamp.go
@@ -3,8 +3,8 @@ package utils
 import "time"
 
 func ParseTimestampToIsoFormat(epochtime int64) string {
-	// Convert the int64 epoch time to a time.Time object
-	t := time.Unix(epochtime, 0)
+	// Convert the int64 epoch time to a time.Time object in UTC
+	t := time.Unix(epochtime, 0).UTC()
 	// Convert the time.Time object to a string in ISO8601 format
 	return t.Format(time.RFC3339)
 }
